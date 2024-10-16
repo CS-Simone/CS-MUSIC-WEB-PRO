30d53e6178c55e2d975c11aeebc8a8e498b9c689
Simone <simon@cursedbots.xyz>
2024-10-15 21:08:08 -0500

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,27 +1,30 @@
 const connectToDatabase = require('./db');
-const Song = require('../SongModel');
-const Album = require('../AlbumModel');
+const Song = require('../models/SongModel');
+const Album = require('../models/AlbumModel');
 const { Readable } = require('stream');
+const auth = require('./auth');
 
 module.exports = async (req, res) => {
   const { gfs } = await connectToDatabase();
   
   if (req.method === 'POST') {
-    const { title, authors, albumId, releaseDate, length } = req.body;
-    const songFile = req.files.songFile;
+    auth(req, res, async () => {
+      const { title, authors, albumId, releaseDate, length } = req.body;
+      const songFile = req.files.songFile;
 
-    const readStream = new Readable();
-    readStream.push(songFile.buffer);
-    readStream.push(null);
+      const readStream = new Readable();
+      readStream.push(songFile.buffer);
+      readStream.push(null);
 
-    const writeStream = gfs.createWriteStream({ filename: songFile.originalname });
-    readStream.pipe(writeStream);
+      const writeStream = gfs.createWriteStream({ filename: songFile.originalname });
+      readStream.pipe(writeStream);
 
-    writeStream.on('close', async (file) => {
-      const album = await Album.findById(albumId);
-      const newSong = new Song({ title, authors: authors.split(','), album: album.name, releaseDate: new Date(releaseDate), length, gridFSFileId: file._id });
-      await newSong.save();
-      res.status(200).json({ message: 'Song uploaded successfully!', song: newSong });
+      writeStream.on('close', async (file) => {
+        const album = await Album.findById(albumId);
+        const newSong = new Song({ title, authors: authors.split(','), album: album.name, releaseDate: new Date(releaseDate), length, gridFSFileId: file._id });
+        await newSong.save();
+        res.status(200).json({ message: 'Song uploaded successfully!', song: newSong });
+      });
     });
   } else if (req.method === 'GET') {
     const albums = await Album.find();
