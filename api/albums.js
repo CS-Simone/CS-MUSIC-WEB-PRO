e62e8b0720a50ad9e8f7e1c64c4ed539e9532f23
Dep1xel <85250250+PixelatedPurple@users.noreply.github.com>
2024-10-16 00:19:00 -0500

    Update albums.js

--- api/albums.js
+++ api/albums.js
@@ -1,6 +1,21 @@
+const Album = require('../models/AlbumModel');
+
 module.exports = async (req, res) => {
-  if (req.method === 'GET') {
-    return res.status(200).json({ message: "Test" });
+  if (req.method === 'POST') {
+    const { name, releaseDate } = req.body;
+    const coverFile = req.files.coverFile;  // Assuming you're using multer to handle file uploads
+
+    const newAlbum = new Album({
+      name,
+      releaseDate: new Date(releaseDate),
+      coverFileData: coverFile.buffer,  // Store the file as Buffer
+      coverFileType: coverFile.mimetype  // Store the file type for headers
+    });
+
+    await newAlbum.save();
+    res.status(200).json({ message: 'Album created!', album: newAlbum });
+  } else if (req.method === 'GET') {
+    const albums = await Album.find();
+    res.status(200).json(albums);
   }
 };
- 
