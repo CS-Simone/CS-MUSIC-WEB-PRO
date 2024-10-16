58b0072c728cb2204d3461d6ef202a79e8e024c8
Simone <simon@cursedbots.xyz>
2024-10-15 21:08:36 -0500

    Update audio.js

--- api/audio.js
+++ api/audio.js
@@ -2,10 +2,9 @@ const connectToDatabase = require('./db');
 
 module.exports = async (req, res) => {
   const { gfs } = await connectToDatabase();
+  const { id } = req.query;
 
-  const { id } = req.query; // GridFS file ID from request
   const readStream = gfs.createReadStream({ _id: id });
-
   readStream.on('error', () => res.status(404).send('File not found'));
   readStream.pipe(res);
 };
