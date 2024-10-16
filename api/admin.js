12070a2751cac1f40e4f84d386fd4112d24a7630
Simone <simon@cursedbots.xyz>
2024-10-15 22:47:27 -0500

    Delete api/admin.js
    No longer required

--- api/admin.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const isAuthenticated = localStorage.getItem("isAuthenticated");
-
-  if (!isAuthenticated) {
-    // Redirect to login if not authenticated
-    window.location.href = "/login.html";
-  }
