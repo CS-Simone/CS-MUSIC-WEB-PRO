15076cc6f916b43b4874cc2c0dbd2741560c5598
Dep1xel <85250250+PixelatedPurple@users.noreply.github.com>
2024-10-15 23:23:05 -0500

    Update AlbumModel.js

--- models/AlbumModel.js
+++ models/AlbumModel.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
 
-const AlbumSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  releaseDate: { type: Date, required: true },
-  coverFileId: { type: mongoose.Schema.Types.ObjectId, required: true }
+const albumSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+  coverFileId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.models.Album || mongoose.model('Album', AlbumSchema);
+// Create the Album model from the schema
+const Album = mongoose.model('Album', albumSchema);
+
+module.exports = Album;
