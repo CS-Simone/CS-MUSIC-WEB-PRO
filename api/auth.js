46317bb8f5c62b99164e72887fba7d64bb542f7e
Dep1xel <85250250+PixelatedPurple@users.noreply.github.com>
2024-10-15 21:46:55 -0500

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,13 +1,7 @@
 module.exports = (req, res, next) => {
-  const auth = { login: process.env.ADMIN_USERNAME, password: process.env.ADMIN_PASSWORD };
-
-  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
-  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
-
-  if (login && password && login === auth.login && password === auth.password) {
-    return next();
+  if (req.session && req.session.loggedIn) {
+    next();
+  } else {
+    res.status(401).json({ message: 'Unauthorized access. Please log in.' });
   }
-
-  res.set('WWW-Authenticate', 'Basic realm="401"');
-  res.status(401).send('Authentication required.');
 };
