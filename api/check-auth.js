f4300173242840115d31cdc05b52ca969b9d5002
Simone <simon@cursedbots.xyz>
2024-10-15 22:47:05 -0500

    Delete api/check-auth.js
    No longer required

--- api/check-auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  if (req.session && req.session.loggedIn) {
-    res.status(200).json({ loggedIn: true });
-  } else {
-    res.status(401).json({ loggedIn: false });
-  }
-};
